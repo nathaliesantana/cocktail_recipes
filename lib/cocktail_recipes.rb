ed431929c705b6bd72ee8326501f5395fa911970
Nathalie Santana <nathalie.scruz@gmail.com>
2018-08-29 18:20:35 +0000

    Files created under cocktails_recipes.

--- lib/cocktail_recipes.rb
+++ lib/cocktail_recipes.rb
@@ -1,5 +1,12 @@
-require "cocktail_recipes/version"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
 module CocktailRecipes
   # Your code goes here...
 end
+
+require_relative "./cocktail_recipes/recipes"
+require_relative "./cocktail_recipes/scraper"
+require_relative "./cocktail_recipes/version"
+require_relative "./cocktail_recipes/cli"
---
+++ lib/cocktail_recipes/cli.rb
@@ -0,0 +1,48 @@
+class CocktailRecipes::Cli
+  attr_accessor :cocktails_list
+
+  @@cocktails_list = ["cocktail 1", "cocktail 2", "cocktail 3" ]
+
+  def welcome
+    # SeasonRecipes::Scraper.new("https://www.foodandwine.com/cocktail-recipes").scrape
+    puts "Hi, Welcome to Reinvented Classic Cocktails"
+    main_menu
+  end
+
+  def main_menu
+    puts "Here is a list of all of our cocktails:"
+    puts display_cocktails_list
+    puts "Type the number of the cocktail you would like to learn."
+    input = gets.strip.to_i
+    if (1..@cocktails_list.length).include? (input)
+      puts @cocktails_list[input-1]
+      menu_options
+    else
+      invalid
+    end
+  end
+
+  def menu_options
+    puts "Would you like to see another recipe?"
+    input = gets.strip.downcase
+    input == "yes" ? main_menu : good_bye
+  end
+
+  def good_bye
+    puts "Thank you for your visit."
+    puts "You're welcome to come back anytime!"
+  end
+
+  def invalid
+    puts "This is not an option.!!!!!!!!"
+    main_menu
+  end
+
+  def display_cocktails_list
+    @@cocktails_list.each.with_index(1) do |c, i|
+      puts "#{i}. #{c}"
+    end
+  end
+
+
+end
